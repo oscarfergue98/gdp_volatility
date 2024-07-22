ae5affc056964439f1ec94a76a55087cdecdb77c
oscarfergue98 <98018048+oscarfergue98@users.noreply.github.com>
2024-07-22 20:46:00 +0200

    Update main.R

--- main.R
+++ main.R
@@ -21,20 +21,19 @@ gdp_data <- raw_gdp_data %>%
     geo, TIME_PERIOD, OBS_VALUE
     ) %>% 
   dplyr::filter(
-    geo %in% emu_countries
-    ) %>% 
+    geo %in% emu_countries    ) %>% 
   dplyr::mutate(
     geo = dplyr::if_else(geo=="EL", "GR", geo)
   ) %>% 
-  dplyr::group_by(geo) %>% 
-  dplyr::mutate(
-    gdp_cycle = mFilter::hpfilter(100*log(OBS_VALUE), freq = 1600)$cycle) %>% 
-  dplyr::ungroup() %>% 
   dplyr::mutate(
     date = zoo::as.Date(zoo::as.yearqtr(TIME_PERIOD, format = "%Y-Q%q"))
   ) %>% 
-  dplyr::select(geo, date, gdp_cycle) %>% 
-  dplyr::filter(lubridate::year(date)>=2000) %>% 
+  #dplyr::filter(date<=as.Date("2019-04-01"), date>=as.Date("1990-01-01")) %>% 
+  dplyr::group_by(geo) %>% 
+  dplyr::mutate(
+    gdp_cycle = mFilter::hpfilter(100 * log(OBS_VALUE), freq = 1600, type = "lambda")$cycle) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::select(date, geo, gdp_cycle) %>% 
   tidyr::pivot_wider(names_from = geo, values_from = gdp_cycle) %>% 
   dplyr::arrange(date) %>% 
   dplyr::mutate(
@@ -47,22 +46,25 @@ gdp_data <- raw_gdp_data %>%
     LV = dplyr::if_else(lubridate::year(date)<2014, NA, LV),
     LT = dplyr::if_else(lubridate::year(date)<2015, NA, LT),
     HR = dplyr::if_else(lubridate::year(date)<2023, NA, HR)
-  ) %>% 
+  ) %>%
   dplyr::rowwise() %>%
   dplyr::mutate(
-    vol_gdp = log(sd(dplyr::c_across(-matches("date")), na.rm = T))
+    vol_gdp = sd(dplyr::c_across(-matches("date")), na.rm = T),
+    log_vol_gdp = log(sd(dplyr::c_across(-matches("date")), na.rm = T))
     ) %>%
-  dplyr::ungroup() 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(date)
 
 # Create a plot depicting GDP volatility
 gdp_data %>% 
+  dplyr::filter(lubridate::year(date)>=2005) %>% 
   ggplot2::ggplot(aes(x = date, y = vol_gdp)) + 
-  ggplot2::geom_line(col = "darkblue") + 
+  ggplot2::geom_line(col = "black") + 
   ggplot2::theme_light() + 
-  ggplot2::labs(x = "", y = "EMU GDP Volatility (in logs)") + 
+  ggplot2::labs(x = "", y = "EMU GDP Volatility") + 
   ggplot2::geom_vline(xintercept = zoo::as.Date("2008-01-01"), linetype = "dashed") + 
   ggplot2::geom_vline(xintercept = zoo::as.Date("2012-01-01"), linetype = "dashed") + 
-  ggplot2::geom_vline(xintercept = zoo::as.Date("2020-01-01"), linetype = "dashed")
+  ggplot2::geom_vline(xintercept = zoo::as.Date("2020-01-01"), linetype = "dashed") 
 
 # Forecasting exercise 
 
@@ -75,8 +77,12 @@ gdp_data <- data.frame(date = seq(as.Date(gdp_data$date[1]),
   dplyr::left_join(gdp_data %>% dplyr::select(date, vol_gdp), by = "date") %>% 
   dplyr::mutate(
     lag_vol_gdp = dplyr::lag(vol_gdp),
-    vol_gdp_upr = NA, 
-    vol_gdp_lwr = NA)
+    vol_gdp_upr_95 = NA, 
+    vol_gdp_lwr_95 = NA, 
+    vol_gdp_upr_68 = NA, 
+    vol_gdp_lwr_68 = NA, 
+    d_covid = date %in% c(as.Date("2020-04-01"), as.Date("2020-07-01"), 
+                          as.Date("2020-10-01")))
 
 
 first_row_fcast <- which(gdp_data$date == date_first_fcast)
@@ -84,12 +90,15 @@ first_row_fcast <- which(gdp_data$date == date_first_fcast)
 
 for (jj in first_row_fcast:(first_row_fcast + fcast_horizon - 1)) {
   
-  current.model = lm(vol_gdp ~ lag_vol_gdp, gdp_data)
-  current.pred <- predict(current.model, gdp_data[jj, ], interval = "confidence")
+  current.model <- lm(vol_gdp ~ lag_vol_gdp, gdp_data)
+  current.pred <- predict(current.model, gdp_data[jj, ], 
+                          interval = "confidence", level = c(.95, .8))
   
-  gdp_data$vol_gdp[jj] = current.pred[, "fit"]
-  gdp_data$vol_gdp_upr[jj] = current.pred[, "upr"]
-  gdp_data$vol_gdp_lwr[jj] = current.pred[, "lwr"]
+  gdp_data$vol_gdp[jj] <- current.pred[1, "fit"]
+  gdp_data$vol_gdp_upr_95[jj] <- current.pred[1, "upr"]
+  gdp_data$vol_gdp_lwr_95[jj] <- current.pred[1, "lwr"]
+  gdp_data$vol_gdp_upr_68[jj] <- current.pred[2, "upr"]
+  gdp_data$vol_gdp_lwr_68[jj] <- current.pred[2, "lwr"]
   
   
   gdp_data <- gdp_data %>% 
@@ -103,14 +112,14 @@ for (jj in first_row_fcast:(first_row_fcast + fcast_horizon - 1)) {
 # Create a plot depicting the forecast
 
 gdp_data %>% 
-  dplyr::filter(lubridate::year(date)>=2021) %>% 
+  dplyr::filter(lubridate::year(date)>=2022) %>% 
   dplyr::mutate(
     date_str = dplyr::if_else(date>=date_first_fcast, "Forecast", "Historical")
   ) %>% 
   ggplot2::ggplot(aes(x = date)) + 
+  ggplot2::geom_ribbon(aes(x=date, ymax=vol_gdp_upr_95, ymin=vol_gdp_lwr_95), fill="grey", alpha=.3) +
+  ggplot2::geom_ribbon(aes(x=date, ymax=vol_gdp_upr_68, ymin=vol_gdp_lwr_68), fill="grey", alpha=.8) +
   ggplot2::geom_line(aes(y = vol_gdp), col = "darkblue") + 
-  ggplot2::geom_line(aes(y = vol_gdp_upr), col = "darkred", linetype = "dashed") + 
-  ggplot2::geom_line(aes(y = vol_gdp_lwr), col = "darkred", linetype = "dashed") + 
   ggplot2::theme_light() + 
   ggplot2::labs(x = "", y = "EMU GDP Volatility (in logs)") + 
   ggplot2::geom_vline(xintercept = zoo::as.Date("2008-01-01"), linetype = "dashed") + 
